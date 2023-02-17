29a14934c1062bea74dd87b67cf4ca64368ffe21
内田達也 <sopratico0326@gmail.com>
2023-02-17 10:21:47 +0000

    Finish top-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'staticpages/top'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'staticpages#top'
 end
